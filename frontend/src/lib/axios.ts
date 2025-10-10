20ed1a076fe4b06509baec6f7650d51435c2ef5f
Sayan Sen <sayansen0361@gmail.com>
2025-10-10 21:32:17 +0530

    Redeploying...

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/"
+const BASE_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:5000"
+        : import.meta.env.VITE_API_URL;
+
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
